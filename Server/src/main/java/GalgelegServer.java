765e472dd6214becb356e839fb7d0f3bad02fd33
Siff <siff.ravn@gmail.com>
2020-02-14 13:35:34 +0100

    feat: insert GalgelegServer

---
+++ Server/src/main/java/GalgelegServer.java
@@ -0,0 +1,21 @@
+import java.rmi.Naming;
+
+public class GalgelegServer {
+
+    public static void main(String[] args) throws Exception{
+
+        java.rmi.registry.LocateRegistry.createRegistry(1099);
+
+        IGalgeLogik galgeLogik = new GalgelogikImpl();
+        Naming.rebind("rmi://localhost/Hangman", galgeLogik);
+        System.out.println("Galelogik registreret");
+
+//        Brugeradmin brugeradmin = new BrugeradminImpl();
+//        Naming.rebind("rmi://localhost/brugeradmin", brugeradmin);
+//        System.out.println("BrugerAdmin registreret");
+//
+//        Brugeradmin brugeradmin = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+//        Naming.rebind("rmi://localhost/brugeradmin", brugeradmin);
+//        System.out.println("BrugerAdmin registreret");
+    }
+}
