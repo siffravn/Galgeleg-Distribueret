69603bdebeb45d577812c77c9bd28a2b4e6cf271
Siff <siff.ravn@gmail.com>
2020-02-14 13:48:07 +0100

    feat: insert brugerautorisation module in Server

---
+++ Server/src/main/java/brugerautorisation/transport/rmi/Brugeradminserver.java
@@ -0,0 +1,35 @@
+package brugerautorisation.transport.rmi;
+
+import brugerautorisation.server.Brugerdatabase;
+
+import java.io.IOException;
+import java.rmi.Naming;
+
+public class Brugeradminserver
+{
+  /* Efter at det i 3 år har fungeret fint at impl er en lokal variabel dukker dette problem pludselig op:
+   * https://stackoverflow.com/questions/645208/java-rmi-nosuchobjectexception-no-such-object-in-table
+   * Der er ikke skiftet Java-version eller noget andet. Det begynder bare at ske. Velkommen til Distribuerede systemer.
+   */
+  private static BrugeradminImpl impl;
+	public static void main(String[] arg) throws IOException
+	{
+		Brugerdatabase db = Brugerdatabase.getInstans();
+		System.out.println("Publicerer Brugeradmin over RMI");
+		impl = new BrugeradminImpl();
+		impl.db = db;
+		java.rmi.registry.LocateRegistry.createRegistry(1099); // start rmiregistry i server-JVM
+		Naming.rebind("rmi://localhost/brugeradmin", impl);
+		System.out.println("Brugeradmin publiceret over RMI");
+	}
+}
+/* Overfør til server med f.eks.:
+
+cd /home/j/DistribueredeSystemer/DistribueredeSystemer/
+ant -q
+rsync -a dist/* deltagere.html gmail-adgangskode.txt  javabog.dk:DistribueredeSystemer/
+
+// På serveren javabog.dk - start med
+cd DistribueredeSystemer
+java -jar DistribueredeSystemer.jar
+*/
