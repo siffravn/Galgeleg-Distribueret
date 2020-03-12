232eba1447f38d6b01f753e5ad74ae9279528e3f
Siff <siff.ravn@gmail.com>
2020-03-12 11:06:11 +0100

    Implement RESTServer

--- Server/src/main/java/GalgelegRESTServer.java
+++ Server/src/main/java/GalgelegRESTServer.java
@@ -7,20 +7,27 @@ import java.rmi.Naming;
 public class GalgelegRESTServer {
 
     public static Javalin app;
+    public static IGalgeLogik galgeLogik;
 
-    public static void main(String[] args) {
-        runServer();
+    public static void main(String[] args) throws Exception {
+        app = Javalin.create().start(4000);
+        galgeLogik = (IGalgeLogik) Naming.lookup("rmi://localhost/Hangman");
+
+        setupResources();
     }
 
-    public static void runServer(){
-        if (app!=null) return;
-
-        app = Javalin.create().start(4000);
+    public static void setupResources() throws Exception{
 
         app.get("", ctx -> ctx.redirect("/login"));
         app.get("/login", ctx -> login(ctx));
         app.get("/play", ctx -> hangman(ctx));
+        app.get("/play/lives", ctx -> getLives(ctx, galgeLogik));
+        app.get("/play/visibleWord", ctx -> getVisibleWord(ctx, galgeLogik));
+        app.get("/play/usedLetters", ctx -> getUsedLetters(ctx, galgeLogik));
+        app.get("/play/guess", ctx -> guessOnLetter(ctx, galgeLogik));
+        app.get("/play/isFinished", ctx -> isGameFinished(ctx, galgeLogik));
     }
+
 
     // Acting as rmi-client to Javabog.dk
     //http://localhost:4000/login?username=s173998&password=kk29
@@ -45,22 +52,64 @@ public class GalgelegRESTServer {
 
     }
 
-    // Acting as rmi-client to GaglelegRMIServer (localhost)
-    public static void hangman(Context ctx) throws Exception{
-        IGalgeLogik galgeLogik = (IGalgeLogik) Naming.lookup("rmi://localhost/Hangman");
-        String letter = ctx.queryParam("guess");
+    public static void getLives(Context ctx, IGalgeLogik galgeLogik) throws Exception {
+        try {
+            int lives = 7-galgeLogik.getAntalForkerteBogstaver();
+            ctx.html("" + lives);
 
-        if (letter == null){
-            ctx.html(galgeLogik.getSynligtOrd());
-        }else {
-            try {
-                while(!galgeLogik.erSpilletSlut()){
-                    galgeLogik.gætBogstav(letter);
-                    ctx.html(galgeLogik.getSynligtOrd());
-                }
-            }catch (Exception e){
-                ctx.html("An error occurred");
-            }
+        } catch (Exception e){
+            ctx.html("Could not access resource \"Lives\"");
         }
+    }
+
+    public static void getVisibleWord(Context ctx, IGalgeLogik galgeLogik) throws Exception {
+        try {
+            String visibleWord = galgeLogik.getSynligtOrd();
+            ctx.html(visibleWord);
+
+        } catch (Exception e){
+            ctx.html("Could not access resource \"Visible Word\"");
+        }
+    }
+
+    public static void getUsedLetters(Context ctx, IGalgeLogik galgeLogik) throws Exception{
+        try {
+            String usedLetters = galgeLogik.getBrugteBogstaver().toString();
+            ctx.html(usedLetters);
+
+        } catch (Exception e){
+            ctx.html("Could not access resource \"Used Letters\"");
+        }
+    }
+
+    public static void guessOnLetter (Context ctx, IGalgeLogik galgeLogik) throws Exception {
+        try {
+            String letter = ctx.queryParam("letter");
+            galgeLogik.gætBogstav(letter);
+
+            if (galgeLogik.erSidsteBogstavKorrekt()){
+                ctx.html("correct letter");
+
+            } else {
+                ctx.html("incorrect letter");
+            }
+
+        } catch (Exception e){
+            ctx.html("Could not submit query");
+        }
+    }
+
+    public static void isGameFinished(Context ctx, IGalgeLogik galgeLogik) throws Exception{
+        try {
+            boolean isFinished = galgeLogik.erSpilletSlut();
+            ctx.html("" + isFinished);
+
+        } catch (Exception e){
+            ctx.html("Could not access resource \"Is Game Finished\"");
+        }
+    }
+
+    public static void hangman(Context ctx) throws Exception{
+        return;
     }
 }
